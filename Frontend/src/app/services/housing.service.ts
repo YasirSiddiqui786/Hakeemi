c061033fd46b60be459cceecc28c6315d6c3fbc2
Yasir <technoyasir@gmail.com>
2023-04-17 16:26:37 +0530

    formatted the document

--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -2,14 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HousingService {
+  constructor(private http: HttpClient) {}
 
-constructor(private http: HttpClient) { }
-
-getAllProperties(){
- return this.http.get('data/properties.json')
-}
-
+  getAllProperties() {
+    return this.http.get('data/properties.json');
+  }
 }
